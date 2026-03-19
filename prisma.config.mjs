76ec707280f0377e2f432abe36b9d4150828512f
Qarib2004 <garibalisultanov@gmail.com>
2026-03-19 14:18:45 +0400

    write  project build pc

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,12 +1,21 @@
 // import "dotenv/config";
+// import { defineConfig } from "prisma/config";
+
+
 import { defineConfig } from "prisma/config";
-
-
-
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  datasource: {
+    adapter: new PrismaNeon(process.env.DATABASE_URL),
+  },
 });
+
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+// });
 
 // export default defineConfig({
 //   schema: "prisma/schema.prisma",
