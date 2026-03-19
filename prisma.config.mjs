25142c2a014eb66e925e7833cea9667eedfc6ab9
Qarib2004 <garibalisultanov@gmail.com>
2026-03-19 14:14:11 +0400

    write  project build pc

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,12 +1,19 @@
-import "dotenv/config";
+// import "dotenv/config";
 import { defineConfig } from "prisma/config";
+
+
+
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    seed: "tsx prisma/seed.ts", // просто строка с командой запуска
-  },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
 });
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     seed: "tsx prisma/seed.ts", // просто строка с командой запуска
+//   },
+//   datasource: {
+//     url: process.env.DATABASE_URL,
+//   },
+// });
